4bfd445ec7d9011232d2ba9840de5f63b477f599
KarenGuyumjyan <karen.guyumjyan99@gmail.com>
2025-08-29 02:55:35 +0400

    Create Filters, TopNavigationBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,27 @@
+import { Container, Title } from '@/components/shared'
+import { Filters } from '@/components/shared/filters'
+import { TopBar } from '@/components/shared/top-bar'
+
 export default function Home() {
-  return <div>Hello</div>
+  return (
+    <>
+      <Container className='mt-10'>
+        <Title text='All Pizzas' size='lg' className='font-extrabold' />
+      </Container>
+      <TopBar />
+
+      <Container className='mt-10'>
+        <div className='flex gap-12.5'>
+          {/* Filters */}
+          <div className='w-[250px]'>
+            <Filters />
+          </div>
+
+          {/* List of items */}
+          <div className='flex-1'>
+          </div>
+        </div>
+      </Container>
+    </>
+  )
 }
--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -13,14 +13,14 @@ type PropsTypes = {
 export const Header = ({ className }: PropsTypes) => {
   return (
     <header className={cn('border border-b', className)}>
-      <Container className='flex items-center justify-between p-12'>
+      <Container className='flex items-center justify-between py-12'>
         <Link href='/'>
           <div className='flex items-center gap-4'>
             <Image src='/logo.png' alt='Logo' width={35} height={35} />
             <div>
               <h1 className='text-2xl uppercase font-black'>DoDo Pizza</h1>
               <p className='text-sm text-gray-400 leading-3'>
-                вкусней уже некуда
+                {"It couldn't be tastier"}
               </p>
             </div>
           </div>
